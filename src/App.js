f32cc41dbce3d86a985e051dde89372a8a987819
Jayaraman Pancharathinam <jayaram.mailbox@gmail.com>
2022-06-03 15:13:57 +0900

    Base solution commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import "./App.css";
+import { useState } from "react";
+import Chat from "./components/Chat/Chat";
+import LoginDialog from "./components/LoginDialog";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [nickname, setNickname] = useState("");
+ const [loggedIn, setLoggedIn] = useState(false);
+
+ const handleNicknameChange = (event) => {
+   setNickname(event.target.value.trim());
+ };
+
+ const handleNicknameSubmit = (e) => {
+   if (!nickname.length) return;
+
+   e.preventDefault();
+
+   setLoggedIn(true);
+ };
+
+ return (
+   <div className="main-div">
+     {!loggedIn ? (
+       <LoginDialog
+         nicknameChange={handleNicknameChange}
+         nicknameSubmit={handleNicknameSubmit}
+       />
+     ) : (
+       <Chat nickname={nickname} />
+     )}
+   </div>
+ );
 }
 
 export default App;
